44e55b0eff1b3cc06126714186b8dc3de054bc3a
Ryo Saeba <52582081+saebaxyz@users.noreply.github.com>
2019-07-06 15:56:26 +0700

    Add files via upload

---
+++ y.py
@@ -0,0 +1,8 @@
+#var inf , open file1
+with open("ki.xyz") as inf:
+    data = list(csv.reader(inf, delimiter='    '))
+#sort by first array [0]
+m = sorted(data, key=lambda data_entry: int(data_entry[0]))
+#var outf, open file2
+with open("sorted.txt", "a") as outf:
+    csv.writer(outf, delimiter='    ').writerows(m)
